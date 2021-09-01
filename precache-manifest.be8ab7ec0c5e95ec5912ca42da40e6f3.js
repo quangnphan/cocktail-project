f9268cd658e227af0cb6e474571ccfefaef88c00
quangnphan <qp2194@gmail.com>
2021-09-01 14:38:21 -0500

    Updates

---
+++ precache-manifest.be8ab7ec0c5e95ec5912ca42da40e6f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90b5c4fffbf84608481ff613b1efeabb",
+    "url": "/cocktail-project/index.html"
+  },
+  {
+    "revision": "d31266fbb4eb9371021f",
+    "url": "/cocktail-project/static/css/main.5c529f91.chunk.css"
+  },
+  {
+    "revision": "e1f6309a6f0d3e350015",
+    "url": "/cocktail-project/static/js/2.72303b8e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cocktail-project/static/js/2.72303b8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d31266fbb4eb9371021f",
+    "url": "/cocktail-project/static/js/main.5dfabfa3.chunk.js"
+  },
+  {
+    "revision": "5e0d0376b5fa2b84059a",
+    "url": "/cocktail-project/static/js/runtime-main.652d3dba.js"
+  },
+  {
+    "revision": "9a3d26451b294fc390aee4ae147a45c7",
+    "url": "/cocktail-project/static/media/logo.9a3d2645.svg"
+  }
+]);
